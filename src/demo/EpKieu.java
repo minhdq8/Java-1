cb45bac32eb823e8e6af7ab13acd383c682ebcc3
minhdq8 <minhdq01003@gmail.com>
2021-05-17 15:54:18 +0700

    "demo2"

---
+++ src/demo/EpKieu.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author minhdq
+ */
+public class EpKieu {
+
+    public static void main(String[] args) {
+//        int a = 9;
+//        double b = 11.1, ccccc = 10;
+//
+//        a = (int) b;
+//        System.out.println(a);
+//        System.out.println(ccccc);
+//
+//        Scanner _sc = new Scanner(System.in);
+//        String x = "34";// 34 + enter = string
+//        String y = "12";
+        // ép chuỗi về KDL nguyên thủy
+//        try {
+//            System.out.print("Tuổi bạn: ");
+//            int _tuoiBan = Integer.parseInt(_sc.nextLine());
+//        } catch (Exception e) {
+//            System.out.println("Tuổi phải là số !!!");
+//        }
+//
+//        System.out.print("Tên: ");
+//        String _ten = _sc.nextLine();
+//
+//        System.out.print("Vòng 3 NY: ");
+//        double _tuoiNY = Double.parseDouble(_sc.nextLine());
+        /**/
+        int a = 10, b = 20, c = 50;
+        
+        int max = a > b ? a : b;
+        int max2 = max > c ? max : c ;
+        
+        System.out.println(max2);
+        
+        
+    }
+
+}
