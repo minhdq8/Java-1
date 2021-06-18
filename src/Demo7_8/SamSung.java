c790ed385f13fcb13d089fabb39e684f533dd902
minhdq8 <minhdq01003@gmail.com>
2021-06-18 07:26:35 +0700

    "demo ke thua"

---
+++ src/Demo7_8/SamSung.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Demo7_8;
+
+/**
+ *
+ * @author minhdq
+ */
+public class SamSung extends DienThoai{
+    
+}
---
+++ src/Demo7_8/fileMain.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Demo7_8;
+
+/**
+ *
+ * @author minhdq
+ */
+public class fileMain {
+    public static void main(String[] args) {
+        TiepThi tt1 = new TiepThi(10, "minh", "001", 9000);
+        tt1.getThue();
+    }
+}
