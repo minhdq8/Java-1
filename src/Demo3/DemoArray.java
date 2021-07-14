3f22b001e9ec18511de3c859f3b0f028e87a5dbe
minhdq8 <minhdq01003@gmail.com>
2021-07-14 10:53:14 +0700

    "demoArray"

---
+++ src/Demo3/DemoArray.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Demo3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author minhdq
+ */
+public class DemoArray {
+
+    static Scanner _sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+//        int [] tuoi = {12,34,64,23,23,12,11,1,4,64,84,342,63,243,1,3,73,3};
+//        tuoi[0] = 20;
+//        String hoTen[] = {"ok", "minh"};
+//        System.out.println(hoTen[1]);
+//        System.out.println(tuoi[0] * 2 + 10);
+//        System.out.println("Size tuổi: " + tuoi.length);
+//        System.out.println("Tuổi cuối cùng: " + tuoi[tuoi.length -1] );
+        
+        //b1 nhập size của mảng
+        System.out.print("Mời nhập size của mảng: ");
+        int _size = Integer.parseInt(_sc.nextLine());
+        //b2 khai bái, khởi tạo mảng
+        int _tuoi[] = new int[_size];
+        //b3 nhập từng phần tử cho mảng
+        for (int i = 0; i < _tuoi.length; i++) {
+            System.out.printf("Nhập phần tử thứ %d : ", i + 1);
+            _tuoi[i] = Integer.parseInt(_sc.nextLine());
+        }
+        //b4 xuất mảng vừa nhập
+        System.out.println("Mảng vừa nhập là:");
+        for (int i = 0; i < _tuoi.length; i++) {
+            System.out.println(_tuoi[i]);
+        }
+
+    }
+}
--- src/demo2/Ham_Function.java
+++ src/demo2/Ham_Function.java
@@ -10,10 +10,16 @@ import java.util.Scanner;
 /**
  *
  * @author minhdq
+ * 1. Tham số truyền vào
+ * 2. Kiểu dữ liệu trả về
+ * 3. Biến toàn cục, cục bộ
+ * 4. Nạp chồng hàm (cùng tên but khác nhau về tham số truyền vào)
+ * 
  */
 public class Ham_Function {
     static Scanner sc = new Scanner(System.in);
     static int a;
+    static int soThu2;
     public static void main(String[] args) {
         
         System.out.print("Mời nhập số 1: ");
@@ -21,22 +27,31 @@ public class Ham_Function {
         System.out.print("Mời nhập số 2: ");
         int soThu2 = Integer.parseInt(sc.nextLine());
 //        tinhTong();
-//        tinhTong(soThu2, soThu1);
-//        tinhTong(10.2, 9);
+//        tinhTong(a, 20);
+//                //10    10
+//        tinhTong(10.1, (int)9.9);
     
         System.out.println(tinhHieu() * 2);
-        System.out.println(tinhHieu(123.43,111.11));
+        tinhTong(tinhHieu(), a);
+//        System.out.println(tinhHieu(123.43,111.11));
     }
-
+    
+    public static void xuatTT(){
+        System.out.println("in 2 giá trị vừa nhập");
+        System.out.println(a);
+        System.out.println(soThu2);
+    }
+    
     // 1. ko có KDL trả về, ko có tham số truyền vào
     public static void tinhTong() {
         int a = 10, b = 20;
         System.out.println("Tổng" + (a + b));
     }
     // 2. ko có KDL trả về, có tham số truyền vào
-    // thử tự của tham số truyền vào
+    // thứ tự của tham số truyền vào
+                                // 10       10
     public static void tinhTong(int so1, int so2) {
-        System.out.println(so1);
+        System.out.println(so1);//10
         System.out.println("Tổng: " + (so1 + so2));
     }// KDL của tham số = KDL của truyền vào
 
@@ -58,5 +73,8 @@ public class Ham_Function {
     // 4. có KDL và có tham số
     public static double tinhHieu(double a, double b){
         return (a - b);
+    }
+    public static String showTen(){
+        return "Chuổi tên Minh mắt mờ vì xem bóng :D ";
     }
 }
